ff9cda600ced0fb61ce8c313db4eaff57d814819
Zelnatal <elnatan.honorato@gmail.com>
2023-07-26 14:56:28 -0300

    .

--- my-project/routes/web.php
+++ my-project/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Livewire\Index;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/count', function () {
+    return view('count');
+});
+
+Route::get('/site', Index::class);
